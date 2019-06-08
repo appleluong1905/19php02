97d35c095ea48524e4491e53b2c1ab141399a4f7
Chad Luong <apple.luong1905@gmail.com>
2019-06-08 15:55:02 +0700

    update

--- session10/js/string.js
+++ session10/js/string.js
@@ -22,3 +22,58 @@ console.log(name.toUpperCase());
 
 //viet thuong chuoi ky tu
 console.log(name.toLowerCase());
+
+// Bài tập string javascript:
+// 1. Cho 1 biến chứa họ tên đầy đủ. Ví dụ: Tran Van Tuan
+// 2. Kiểm tra xem họ tên hôm bao nhiêu ký tự?
+// 3. Tách họ, tên đệm, tên ra?
+// 4. Viết hoa tên đệm lên và in ra đầy đủ 
+// 5. Kiểm tra tên có ký tự n không? có và không có phân 
+// biệt hoa thường
+// 6. Thay thế tên bằng 19PHP02 và in ra đầy đủ
+// 7. Kiểm tra trong họ tên gồm bao nhiêu ký tự n?
+// 8. Bôi vàng ký tự a trong tên nếu có?
+document.write("<br>");
+function replaceSpace(replaceString) {
+	var newreplaceString;
+	while (replaceString.indexOf(' ') != -1) {
+			newreplaceString = replaceString.replace(" ", "");
+			replaceString = newreplaceString;
+	}
+	return replaceString;
+}
+// Tran Van Minh Tuan
+function splitFirstName(name) {
+	// cat ho ra la: cat trong ho va ten tinh tu dau cho den khoang
+	// trong dau tien
+	var firstSpace = name.indexOf(' ');
+	var firstName = name.substring(0, firstSpace);
+	return firstName;
+}
+function splitMiddleName(name) {
+	// cat ten dem ra la: cat trong ho va ten tinh tu khoang
+	// trong dau tien cho den khoang trong cuoi cung
+	var firstSpace = name.indexOf(' ');
+	var lastSpace = name.lastIndexOf(' ');
+	var middleName = name.substring(firstSpace, lastSpace);
+	return middleName;
+}
+function splitLastName(name) {
+	// cat ten ra la: cat trong ho va ten tinh tu khoang
+	// trong cuoi cung cho den cuoi ho ten
+	var lastSpace = name.lastIndexOf(' ');
+	var lastName = name.substring(lastSpace, name.length);
+	return lastName;
+}
+var myName = prompt("Please enter your name", "Luong Minh Canh");
+document.write("1. Ho va ten cua ban la: " + myName);
+
+document.write("<br>2. So ky tu trong ho va ten cua ban la: "
+ + replaceSpace(myName).length);
+document.write("<br>3. Ho cua ban la: "
+ + splitFirstName(myName));
+document.write("<br>Ten cua ban la: "
+ + splitLastName(myName));
+document.write("<br>Ten dem cua ban la: "
+ + splitMiddleName(myName));
+
